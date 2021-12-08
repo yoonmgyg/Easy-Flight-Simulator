1e7fa4396e791b12219465cc8575e8a3f378e564
yoonmgyg <yoonmgyg@gmail.com>
2021-12-07 23:01:00 -0500

    added geocoder library and reorganized files

---
+++ cpr/cpr/CMakeFiles/cpr.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/zeodex/Downloads/cpp-opencage-geocoder/cpr/cpr/auth.cpp" "cpr/cpr/CMakeFiles/cpr.dir/auth.cpp.o" "gcc" "cpr/cpr/CMakeFiles/cpr.dir/auth.cpp.o.d"
+  "/home/zeodex/Downloads/cpp-opencage-geocoder/cpr/cpr/cookies.cpp" "cpr/cpr/CMakeFiles/cpr.dir/cookies.cpp.o" "gcc" "cpr/cpr/CMakeFiles/cpr.dir/cookies.cpp.o.d"
+  "/home/zeodex/Downloads/cpp-opencage-geocoder/cpr/cpr/cprtypes.cpp" "cpr/cpr/CMakeFiles/cpr.dir/cprtypes.cpp.o" "gcc" "cpr/cpr/CMakeFiles/cpr.dir/cprtypes.cpp.o.d"
+  "/home/zeodex/Downloads/cpp-opencage-geocoder/cpr/cpr/digest.cpp" "cpr/cpr/CMakeFiles/cpr.dir/digest.cpp.o" "gcc" "cpr/cpr/CMakeFiles/cpr.dir/digest.cpp.o.d"
+  "/home/zeodex/Downloads/cpp-opencage-geocoder/cpr/cpr/error.cpp" "cpr/cpr/CMakeFiles/cpr.dir/error.cpp.o" "gcc" "cpr/cpr/CMakeFiles/cpr.dir/error.cpp.o.d"
+  "/home/zeodex/Downloads/cpp-opencage-geocoder/cpr/cpr/multipart.cpp" "cpr/cpr/CMakeFiles/cpr.dir/multipart.cpp.o" "gcc" "cpr/cpr/CMakeFiles/cpr.dir/multipart.cpp.o.d"
+  "/home/zeodex/Downloads/cpp-opencage-geocoder/cpr/cpr/parameters.cpp" "cpr/cpr/CMakeFiles/cpr.dir/parameters.cpp.o" "gcc" "cpr/cpr/CMakeFiles/cpr.dir/parameters.cpp.o.d"
+  "/home/zeodex/Downloads/cpp-opencage-geocoder/cpr/cpr/payload.cpp" "cpr/cpr/CMakeFiles/cpr.dir/payload.cpp.o" "gcc" "cpr/cpr/CMakeFiles/cpr.dir/payload.cpp.o.d"
+  "/home/zeodex/Downloads/cpp-opencage-geocoder/cpr/cpr/proxies.cpp" "cpr/cpr/CMakeFiles/cpr.dir/proxies.cpp.o" "gcc" "cpr/cpr/CMakeFiles/cpr.dir/proxies.cpp.o.d"
+  "/home/zeodex/Downloads/cpp-opencage-geocoder/cpr/cpr/session.cpp" "cpr/cpr/CMakeFiles/cpr.dir/session.cpp.o" "gcc" "cpr/cpr/CMakeFiles/cpr.dir/session.cpp.o.d"
+  "/home/zeodex/Downloads/cpp-opencage-geocoder/cpr/cpr/ssl_options.cpp" "cpr/cpr/CMakeFiles/cpr.dir/ssl_options.cpp.o" "gcc" "cpr/cpr/CMakeFiles/cpr.dir/ssl_options.cpp.o.d"
+  "/home/zeodex/Downloads/cpp-opencage-geocoder/cpr/cpr/timeout.cpp" "cpr/cpr/CMakeFiles/cpr.dir/timeout.cpp.o" "gcc" "cpr/cpr/CMakeFiles/cpr.dir/timeout.cpp.o.d"
+  "/home/zeodex/Downloads/cpp-opencage-geocoder/cpr/cpr/util.cpp" "cpr/cpr/CMakeFiles/cpr.dir/util.cpp.o" "gcc" "cpr/cpr/CMakeFiles/cpr.dir/util.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
